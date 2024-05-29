97086f976a8818dcfbd402fb2fba7934f3e3ade6
Ricardo Matsumura <fuzilo@hotmail.com>
2024-05-29 13:51:35 -0300

    Initial commit

---
+++ src/test/java/VeridiOculi/PrototpoArduino/PrototpoArduinoApplicationTests.java
@@ -0,0 +1,13 @@
+package VeridiOculi.PrototpoArduino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototpoArduinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
